788a887fdd84a0193187117481c8c3c1f0852455
gabrielpda15 <gabriel.pda15@gmail.com>
2020-05-25 17:36:43 -0300

    Criação de projetos base

---
+++ Aprese.Repository[/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Aprese.Repository_
+{
+    public class Class1
+    {
+    }
+}
--- Aprese.WebAPI/Startup.cs
+++ Aprese.WebAPI/Startup.cs
@@ -2,19 +2,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Aprese.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 
 namespace Aprese.WebAPI
 {
     public class Startup
     {
+        public const string CONN_STRING = "Default";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -22,13 +27,19 @@ namespace Aprese.WebAPI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<ApreseContext>(options =>
+            {
+                options.UseMySql(Configuration.GetConnectionString(CONN_STRING), o =>
+                {
+                    o.ServerVersion(new Version(8, 0, 0), ServerType.MySql);
+                });
+            });
+
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
