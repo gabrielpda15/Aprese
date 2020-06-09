8f0b6b44dc2dfd125df213d9d420eb91e50e6861
Gabriel Pupim <gabriel.pda15@gmail.com>
2020-06-09 15:00:20 -0300

    Inclusão do carregando das dependencias

---
+++ Aprese.Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Aprese.DependencyInjection;
+using Aprese.Repository.Events;
+using Aprese.Repository.Interfaces;
+using Microsoft.AspNetCore.Hosting;
+using System.Linq;
+using System.Reflection;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddEventsAndRules(this IServiceCollection services)
+        {
+            return services.AddEvents().AddRules();
+        }
+
+        private static IServiceCollection AddEvents(this IServiceCollection services)
+        {
+            var types = WebHostBuilderExtensions.Assemblies.SelectMany(x => x.GetTypes())
+                .Where(x => x.GetInterfaces().FirstOrDefault()?.GetCustomAttribute<InjectableEventAttribute>() != null);
+
+            foreach (var type in types)
+            {
+                services.AddTransient(type.GetInterfaces().FirstOrDefault(), type);
+            }
+
+            return services;
+        }
+
+        private static IServiceCollection AddRules(this IServiceCollection services)
+        {
+            var types = WebHostBuilderExtensions.Assemblies.SelectMany(x => x.GetTypes())
+                .Where(x => x.GetInterfaces().FirstOrDefault()?.GetCustomAttribute<InjectableRuleAttribute>() != null);
+
+            foreach (var type in types)
+            {
+                services.AddTransient(type.GetInterfaces().FirstOrDefault(), type);
+            }
+
+            return services;
+        }
+    }
+}
---
+++ Aprese.Extensions/WebHostBuilderExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Microsoft.AspNetCore.Hosting
+{
+    public static class WebHostBuilderExtensions
+    {
+        internal static List<Assembly> Assemblies { get; private set; } = new List<Assembly>();
+
+        public static IWebHostBuilder LoadAllDllsBinFolder(this IWebHostBuilder builder)
+        {
+            string binPath = AppDomain.CurrentDomain.BaseDirectory;
+
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies()
+                .Where(x => x.GetName().Name.StartsWith("Aprese"));
+            Assemblies.AddRange(assemblies);
+
+            foreach (string dll in Directory.GetFiles(binPath, "*.dll", SearchOption.AllDirectories))
+            {
+                try
+                {
+                    if (!assemblies.Select(x => x.GetName().Name).Contains(Path.GetFileNameWithoutExtension(dll)) &&
+                        Path.GetFileNameWithoutExtension(dll).StartsWith("Aprese"))
+                    {
+                        var assembly = Assembly.LoadFile(dll);
+                        Assemblies.Add(assembly);
+                    }
+                }
+                catch (FileLoadException) { } 
+                catch (BadImageFormatException) { }
+            }
+
+            return builder;
+        }
+    }
+}
