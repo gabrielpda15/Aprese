ff7aaabf04e3042a9c32f9979862615015fcd4f1
Pablo Rodrigues <laurindopablo@gmail.com>
2020-06-09 11:21:52 -0300

    Criando a classe base do modelo para Cliente

---
+++ Aprese.Models/Base/Client.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Aprese.Models.Base
+{
+    public class Client
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public virtual int Id { get; set; }
+
+        [DataType("varchar")]
+        [StringLength(70)]
+        [ScaffoldColumn(false)]
+        public string Name { get; set; }
+
+        [DataType("varchar")]
+        [StringLength(30)]
+        [ScaffoldColumn(false)]
+        public string Naturality { get; set; }
+
+        [DataType("varchar")]
+        [StringLength(15)]
+        [ScaffoldColumn(false)]
+        public string CPF { get; set; }
+
+        [DataType("varchar")]
+        [StringLength(19)]
+        [ScaffoldColumn(false)]
+        public string CNPJ { get; set; }
+    }
+}
