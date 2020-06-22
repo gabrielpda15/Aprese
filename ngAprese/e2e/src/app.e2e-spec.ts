4ca974c19eed4f705cc5019f21ef6724652b685c
Gabriel Pupim <gabriel.pda15@gmail.com>
2020-06-22 09:28:40 -0300

    Conversão CSS para SCSS

--- ngAprese/e2e/src/app.e2e-spec.ts
+++ ngAprese/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('ngAprese app is running!');
+    expect(page.getTitleText()).toEqual('ng-aprese app is running!');
   });
 
   afterEach(async () => {
